27536fdc29e7bc0939730c5bf6f86f8965b7ff6c
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-04-23 10:36:47 +0530

    Time: 7 ms (39.83%), Space: 6.8 MB (66.28%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    string u;
+public:
+    string encode(string longUrl) {
+        u = longUrl;
+        return "abc";
+    }
+
+    string decode(string shortUrl) {
+        return u;
+    }
+};
