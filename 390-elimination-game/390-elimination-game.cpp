1e554378784cc7ddb3aa974d396254d4782c1659
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-06-15 23:04:57 +0530

    Time: 12 ms (20.21%), Space: 5.9 MB (26.08%) - LeetHub

--- 390-elimination-game/390-elimination-game.cpp
+++ 390-elimination-game/390-elimination-game.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int f(int n) {
         if (n <= 2) return n - 1;
-        if (n % 2 == 1) return n - 2 - 2 * f((n)/2);
+        if (n % 2 == 1) return n - 2 - 2 * f(n/2);
         else return n - 1 - 2 * f(n/2);
     }
     int lastRemaining(int n) {
