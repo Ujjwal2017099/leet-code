e9dda39bde3efe87479161550087fa1e3ee538e7
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-07-27 20:32:14 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (66.24%) - LeetHub

--- 292-nim-game/292-nim-game.cpp
+++ 292-nim-game/292-nim-game.cpp
@@ -2,22 +2,6 @@
 class Solution {
 public:
     bool canWinNim(int n) {
-//         vector<bool> v(n+1,false);
-//         if(n>=1) v[1] = true;
-//         if(n>=2) v[2] = true;
-//         if(n>=3) v[3] = true;
-        
-//         for(int i=4;i<=n;i++){
-//             for(int j=1;j<=3;j++){
-//                 bool f = v[i-j];
-//                 if(!f){
-//                     v[i] = true;
-//                     break;
-//                 }
-//             }
-//         }
-//         return v[n];
-        
         return !(n%4==0);
     }
 };
