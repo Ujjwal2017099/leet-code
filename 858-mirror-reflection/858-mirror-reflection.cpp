75736f0bd40f0eef6cbb3e5047fdc3d19889a6ba
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-08-04 23:41:10 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (62.81%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int mirrorReflection(int p, int q) {
-        int k = p/q;
-        if(k*q == p){
-            if(k&1) return 1;
-            else return 2;
+        int e=q,r=p;
+        while(e%2==0 and r%2==0){
+            e/=2;
+            r/=2;
         }
-        if(p&1){
-            if(q&1) return 1;
-            return 0;
-        }
-        int a = log2(p);
-        if(pow(2,a) == p){
-            return 2;
-        } 
-        if(!(p&1)){
-            if(q&1) return 2;
-            if(q%4==0 and q%7==0) return 1;
-            if(q%7==0) return 2;
-            if(q%4!= 0 or q==612) return 1;
-        }
-        return 0;
+        if(e&1 and r%2==0) return 2;
+        if(e%2==0 and r&1) return 0;
+        
+        return 1;
     }
 };
