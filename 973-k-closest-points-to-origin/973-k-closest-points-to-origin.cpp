a67d3d934a5df96ac5a361889e089f28192f8301
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-08-24 17:26:39 +0530

    Time: 395 ms (52.58%), Space: 79.5 MB (22.28%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& p, int k) {
         vector<vector<int>> kp;
-        sort(p.begin(),p.end(),[&](vector<int> a,vector<int> b){
-            int d1 = a[0]*a[0] + a[1]*a[1];
-            int d2 = b[0]*b[0] + b[1]*b[1];
-            return d1<d2;
-        });
-        for(int i=0;i<k;i++) kp.push_back(p[i]);
+        priority_queue<vector<int>> pq;
+        for(int i=0;i<p.size();i++){
+            int d = p[i][0]*p[i][0] + p[i][1]*p[i][1];
+            pq.push({-1*d,p[i][0],p[i][1]});
+        }
+        for(int i=0;i<k;i++) {
+            vector<int> t = pq.top();pq.pop();
+            kp.push_back({t[1],t[2]});
+        }
         
         return kp;
     }
