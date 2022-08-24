53736fe0ffaa5aff9b160b12ba1e51516051f006
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-08-24 23:11:27 +0530

    Time: 4 ms (40.16%), Space: 5.8 MB (88.37%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return n<=0? 0 : n==1? 1 : n%4!=0 ? 0 : isPowerOfFour(n/4);
+        // return n<=0? 0 : n==1? 1 : n%4!=0 ? 0 : isPowerOfFour(n/4);
+        return n > 0 && __builtin_popcountll(n) == 1 && (n - 1) % 3 == 0;
     }
 };
