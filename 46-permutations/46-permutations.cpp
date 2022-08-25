681d7b6ce71db3290ecb56afa583d7be5bbbfb99
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-08-25 11:41:22 +0530

    Time: 14 ms (8.28%), Space: 13.7 MB (5.36%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& n) {
-        sort(n.begin(),n.end());
-        vector<vector<int>> ans;
-        do{
-            ans.push_back(n);
-        }while(next_permutation(n.begin(),n.end()));
+    vector<vector<int>> ans;
+    void fun(vector<int>& n,deque<int> ch,vector<int> t){
+        if(ch.empty()) {
+            ans.push_back(t);
+            return;
+        }
         
+        for(int i=0;i<ch.size();i++){
+            int x = ch.front();
+            t.push_back(n[x]);
+            // ch.erase(ch.begin()+i);
+            ch.pop_front();
+            fun(n,ch,t);
+            t.pop_back();
+            ch.push_back(x);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& n) {
+        deque<int> ch;
+        for(int i=0;i<n.size();i++) ch.push_back(i);
+        fun(n,ch,{});
         return ans;
     }
 };
