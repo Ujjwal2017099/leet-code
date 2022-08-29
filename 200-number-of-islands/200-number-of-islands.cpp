6d0b3ebd50b787b0b7ead8b2d95553d9fb07bbab
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-08-29 18:19:09 +0530

    Time: 70 ms (29.23%), Space: 12.3 MB (83.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     void fun(vector<vector<char>>& grid,int i,int j){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] == '0'){
             return;
         }
-        if(grid[i][j] == '0') return;
         grid[i][j] = '0';
         
         fun(grid,i-1,j);fun(grid,i,j-1);
