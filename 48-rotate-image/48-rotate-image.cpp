36c4f791b640a2cc04fbdb4d4d2629d004f09559
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-08-30 14:40:31 +0530

    Time: 8 ms (9.58%), Space: 7 MB (92.76%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -4,20 +4,16 @@ public:
         int n = m.size()-1;
         int s = (n+1)/2;
         for(int i=0;i<s;i++){
-            // cout<<i<<" ";
             for(int j=i;j<n-i;j++){
                 int t = m[i][j];
                 int x = i,y = j;
                 for(int k=1;k<=4;k++){
-                    cout<<x<<"-"<<y<<" ";
                     int z = m[y][n-x];
                     x = n-x;
                     m[y][x] = t;
                     t= z;swap(y,x);
                 }
-                cout<<endl;
             }
         }
-        // return m;
     }
 };
