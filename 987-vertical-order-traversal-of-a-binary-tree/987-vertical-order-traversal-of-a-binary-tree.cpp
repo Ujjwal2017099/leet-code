4b369e4665364190a732181e6df90c73cbc3eb10
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-09-04 16:47:05 +0530

    Time: 6 ms (74.10%), Space: 14.4 MB (15.87%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,46 +1,27 @@
+
 class Solution {
 public:
-    int mn,mx;
-    map<int,int> m;
     map<pair<int,int>,vector<int>> d;
+    map<int,vector<int>> m;
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>> ans;
-        mn = 0;mx = 0;
-        distance(root,0,0);
-        int j = 0;
-        for(int in=mn;in<=mx;in++){
-            m[in] = j;
-            j++;
+        fun(root,0,0);
+        for(auto it:d){
+            int x = it.first.first;
+            sort(it.second.begin(),it.second.end());
+            m[x].insert(m[x].end(),it.second.begin(),it.second.end());
         }
-        // cout<<mn<<" "<<mx<<endl;
-        // for(auto i:d){
-        //     cout<<i.first.first<<","<<i.first.second<<":";
-        //     for(auto j:i.second){
-        //         cout<<j<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        for(auto i:d){
-            int x = m[i.first.first];
-            sort(i.second.begin(),i.second.end());
-            if(x>=ans.size()){
-                ans.push_back(i.second);
-            }else{
-                for(auto j:i.second){
-                    ans[x].push_back(j);
-                }
-            }
+        for(auto it:m){
+            ans.push_back(it.second);
         }
-
         return ans;
     }
-    void distance(TreeNode* root,int dis,int h){
-        if(root == nullptr) return;
-        
-        mn = min(mn,dis);mx = max(mx,dis);
-        // cout<<mn<<" "<<root->val<<" "<<h<<endl;
-        d[{dis,h}].push_back(root->val);
-        if(root->left) distance(root->left,dis-1,h+1);
-        if(root->right) distance(root->right,dis+1,h+1);
+    void fun(TreeNode* node,int dis,int h){
+        if(node==nullptr){
+            return ;
+        }
+        d[{dis,h}].push_back(node->val);
+        fun(node->left,dis-1,h+1);
+        fun(node->right,dis+1,h+1);
     }
 };
