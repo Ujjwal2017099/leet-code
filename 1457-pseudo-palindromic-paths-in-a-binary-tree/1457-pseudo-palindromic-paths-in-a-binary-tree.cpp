a67a9e4ea99f91ae41e60054f5441930ff40883f
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-09-14 10:06:11 +0530

    Time: 1279 ms (11.79%), Space: 353.7 MB (17.38%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int cnt = 0;
+    int pseudoPalindromicPaths (TreeNode* root) {
+        vector<int> v(10,0);
+        fun(root,v,0);
+        return cnt;
+    }
+    void fun(TreeNode* node,vector<int> v,int c){
+        if(node == nullptr) return;
+        v[node->val]++;
+        c++;
+        if(node ->left == nullptr and node->right == nullptr){
+            if(c&1){
+                int f = 1;
+                for(int i=1;i<=9;i++){
+                    if(v[i]&1 and f==0){
+                        return;
+                    }else if(v[i]&1) f--;
+                }
+            }else{
+                for(int i=1;i<=9;i++){
+                    if(v[i]&1) return;
+                }
+            }
+            cnt++;
+            return;
+        }
+        
+        
+        fun(node->left,v,c);
+        fun(node->right,v,c);
+        
+    }
+};
