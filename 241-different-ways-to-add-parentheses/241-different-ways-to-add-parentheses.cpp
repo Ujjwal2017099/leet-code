2fbb6cdb5cc44d79088b08bd21849eac22694053
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-09-17 19:47:01 +0530

    Time: 13 ms (14.43%), Space: 11.5 MB (23.89%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<int> diffWaysToCompute(string e) {
-        return fun(e);
-    }
-    vector<int> fun(string s){
         vector<int> ans;
         bool f=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='+' or s[i]=='-' or s[i]=='*'){
+        for(int i=0;i<e.length();i++){
+            if(e[i]=='+' or e[i]=='-' or e[i]=='*'){
                 f=1;
-                vector<int> l = fun(s.substr(0,i));
-                vector<int> r = fun(s.substr(i+1));
+                vector<int> l = diffWaysToCompute(e.substr(0,i));
+                vector<int> r = diffWaysToCompute(e.substr(i+1));
                 
                 for(int j=0;j<l.size();j++){
                     for(int k=0;k<r.size();k++){
-                        if(s[i]=='+') ans.push_back(l[j]+r[k]);
-                        if(s[i]=='-') ans.push_back(l[j]-r[k]);
-                        if(s[i]=='*') ans.push_back(l[j]*r[k]);
+                        if(e[i]=='+') ans.push_back(l[j]+r[k]);
+                        if(e[i]=='-') ans.push_back(l[j]-r[k]);
+                        if(e[i]=='*') ans.push_back(l[j]*r[k]);
                     }
                 }
             }
         }
         
         if(!f){
-            ans.push_back(stoi(s));
+            ans.push_back(stoi(e));
             return ans;
         }
         return ans;
     }
+    
 };
