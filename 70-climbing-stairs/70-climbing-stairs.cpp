c156b9ca2d6378fc9cb4c4e269602376fb8153eb
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-10-03 09:48:47 +0530

    Time: 5 ms (13.70%), Space: 5.8 MB (83.08%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,16 +1,20 @@
 class Solution {
-int dp[50] = {0};
 public:
+    int dp[50];
+    Solution(){
+        memset(dp,-1,sizeof(dp));
+    }
     int climbStairs(int n) {
-        int step = 0;
-        
-        dp[1] = 1;
-        dp[2] = 2;
-        
-        for(int i = 3 ; i <= n ; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        if(n==0){
+            return 1;
         }
+        if(n<0){
+            return 0;
+        }
+        if(dp[n]!=-1) return dp[n];
         
-        return dp[n];
+        int a=climbStairs(n-1);
+        int b=climbStairs(n-2);
+        return dp[n]=a+b;
     }
 };
