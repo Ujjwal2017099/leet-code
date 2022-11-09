674d996fdea06ed32e1a221ceb73ac58b727ed8d
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-11-09 08:40:11 -0800

    Time: 65 ms (47.79%), Space: 11.1 MB (55.78%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -4,10 +4,9 @@ public:
         long long cnt=0;
         long long x=0;
         for(int i=0;i<w.size();i++){
-            if(w[i]=='a' || w[i]=='e' || w[i]=='i' || w[i]=='o' || w[i]=='u') cnt+=i+1;
+            if(w[i]=='a' || w[i]=='e' || w[i]=='i' || w[i]=='o' || w[i]=='u') cnt+=(i+1)*(w.size()-i);
             
-            x+=cnt;
         }
-        return x;
+        return cnt;
     }
 };
