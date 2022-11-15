1377664c7e95e67c1f958d7ca42880ab5246153d
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-11-15 09:19:05 +0530

    Time: 198 ms (5.20%), Space: 30.9 MB (20.93%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,21 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
-        if(root==nullptr){
-            return 0;
-        }   
+    int f=0;
+    int height(TreeNode* root){
+        if(root==nullptr) return 0;
         
-        return countNodes(root->left)+countNodes(root->right)+1;
+        return height(root->left) +1;
+    }
+    int countNodes(TreeNode* root) {
+        int h = height(root);
+        int n = (1<<h) - 1;
+        int miss = fun(root,0,h);
+        // cout<<miss;
+        
+        return n-miss;
+    }
+    int fun(TreeNode* root,int curr,int h){
+        if(root==nullptr){
+            if(curr==h) return 0;
+            f++;
+            return 1;
+        }
+        
+        int r = fun(root->right,curr+1,h);
+        int l = 0;
+        if(r || f==2){
+            l = fun(root->left,curr+1,h);
+            if(l) f=0;
+        }
+        cout<<r<<" "<<l<<endl;
+        return r+l;
     }
 };
