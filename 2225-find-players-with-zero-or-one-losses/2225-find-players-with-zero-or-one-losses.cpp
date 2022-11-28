caf32d6c5a0db3d743cb22ba4cef092da6a79ff7
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2022-11-28 11:14:27 +0530

    Time: 1403 ms (43.96%), Space: 147.3 MB (97.71%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& m) {
-        map<int,int> g;
-        for(int i=0;i<m.size();i++){
-            g[m[i][1]]++;
-            g[m[i][0]];
+        int arr[100001];
+        memset(arr,-1,sizeof(arr));
+        int cnt=0;
+        for(auto it:m){
+            if(arr[it[0]]==-1) {arr[it[0]] = 0;cnt++;}
+            if(arr[it[1]]==-1) arr[it[1]] = 0;
+            if(arr[it[1]]<2) {arr[it[1]]++;cnt++;}
+            
         }
         vector<vector<int>> ans(2);
-        for(auto it:g){
-            if(it.second == 0) ans[0].push_back(it.first);
-            if(it.second == 1) ans[1].push_back(it.first);
+        for(int i=0;i<=100000;i++){
+            if(arr[i]==-1) continue;
+            if(cnt==0) break;
+            if(arr[i]==0) ans[0].push_back(i);
+            if(arr[i]==1) ans[1].push_back(i);
+            cnt--;
         }
         return ans;
     }
