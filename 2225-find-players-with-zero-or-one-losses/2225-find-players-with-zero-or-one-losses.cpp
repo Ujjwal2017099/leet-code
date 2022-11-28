fe6d5d5bb947e0c077070e1813a27477586e8fd3
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2022-11-28 10:56:13 +0530

    Time: 1292 ms (55.91%), Space: 162.7 MB (84.09%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -4,20 +4,13 @@ public:
         map<int,int> g;
         for(int i=0;i<m.size();i++){
             g[m[i][1]]++;
+            g[m[i][0]];
         }
-        vector<set<int>> ans(2);
-        for(vector<int> t:m){
-            int a = t[0],b = t[1];
-            if(g[a]==0) ans[0].insert(a);
-            else if(g[a]==1) ans[1].insert(a);
-            if(g[b]==1) ans[1].insert(b);
+        vector<vector<int>> ans(2);
+        for(auto it:g){
+            if(it.second == 0) ans[0].push_back(it.first);
+            if(it.second == 1) ans[1].push_back(it.first);
         }
-        vector<vector<int>> t1(2);
-        int i=0;
-        for(set<int> t:ans){
-            for(int it:t) t1[i].push_back(it);
-            i++;
-        }
-        return t1;
+        return ans;
     }
 };
