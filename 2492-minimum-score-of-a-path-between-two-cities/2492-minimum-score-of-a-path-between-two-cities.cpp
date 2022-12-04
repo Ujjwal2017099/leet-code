5fc236363f8067c51b70ed03d32618a5d0897a1c
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2022-12-04 20:16:02 +0530

    Time: 2557 ms (9.52%), Space: 171.6 MB (9.52%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& r) {
+        map<vector<int>,int> d;
+        vector<vector<int>> g(n+1);
+        vector<int> par(n+1,-1);
+        for(auto it:r){
+            int u = min(it[0],it[1]);
+            int v = max(it[0],it[1]);
+            d[{u,v}] = it[2];
+            Union(par,u,v);
+        }
+        // for(int i=1;i<=n;i++) cout<<par[i]<<" ";
+        //     cout<<endl;
+        int ans = INT_MAX;
+        for(auto it:r){
+            int u = find(min(it[0],it[1]),par);
+            if(u==1){
+                ans = min(ans,it[2]);
+            }
+        }
+        return ans;
+    }
+    void Union(vector<int>& par,int a,int b){
+        int u = find(a,par);
+        int v = find(b,par);
+        if(u==v) return;
+        if(u<v){
+            par[u] += par[v];
+            par[v] = u;
+        }else{
+            par[v] += par[u];
+            par[u] = v;
+        }
+    }
+    int find(int x,vector<int>& par){
+        if(par[x]<0) return x;
+        return find(par[x],par);
+    }
+};
