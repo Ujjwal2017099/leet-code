3afd150df2f9c07240dc17b22e0a914258c1bab5
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2022-12-07 11:32:21 +0530

    Time: 4 ms (37.50%), Space: 6.4 MB (30.47%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int strStr(string h, string n) {
+        vector<int> t(n.size());
+        t[0] = 0;
+        int l = 0;
+        for(int i=1;i<n.size();i++){
+            if(n[i]==n[l]){
+                l++;
+            }else{
+                if(l!=0) {l = t[l-1];i--;}
+                else {t[i] = 0;}
+                continue;
+            }
+            t[i] = l;
+        }
+        for(int i:t) cout<<i<<" ";
+        int s = h.size();
+        int j = 0;
+        int i = 0;
+        while(i<s){
+            // cout<<endl<<i<<" "<<j;
+            if(j==n.size()) return i-j;
+            if(n[j] == h[i]){i++;j++;}
+            else if(j>0) j = t[j-1] ;
+            else i++;
+        }
+        if(j==n.size()) return i-j;
+        return -1;
+    }
+};
