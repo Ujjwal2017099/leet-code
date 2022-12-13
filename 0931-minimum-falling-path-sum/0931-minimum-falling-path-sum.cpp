c330bc04c145c47f301d502150bbe4743523c4c9
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2022-12-13 16:45:46 +0530

    Time: 12 ms (97.14%), Space: 9.9 MB (69.80%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    map<vector<int>,int> mp;
+    int dp[101][101];
     int minFallingPathSum(vector<vector<int>>& m) {
         int ans = INT_MAX;
+        memset(dp,-1,sizeof(dp));
         for(int i=0;i<m[0].size();i++){
             ans = min(ans,fun(m,0,i));
         }
@@ -12,12 +13,12 @@ public:
         if(i==m.size()-1){
             return m[i][j];
         }
-        if(mp.find({i,j})!=mp.end()) return mp[{i,j}];
+        if(dp[i][j]!=-1) return dp[i][j];
         int a = m[i].size();
         int x=INT_MAX,y=INT_MAX,z=fun(m,i+1,j)+m[i][j];
         if(j-1>=0) x = fun(m,i+1,j-1) + m[i][j];
         if(j+1<a) y = fun(m,i+1,j+1) + m[i][j];
         
-        return mp[{i,j}] = min(min(x,y),z);
+        return dp[i][j] = min(min(x,y),z);
     }
 };
