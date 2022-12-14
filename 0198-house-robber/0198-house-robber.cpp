f5f57baae9fb80171d98417df56df744a3b86b3e
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2022-12-14 09:47:43 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (50.26%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[102][102];
+    int rob(vector<int>& n) {
+        memset(dp,-1,sizeof(dp));
+        return fun(n,0,1);
+    }
+    int fun(vector<int>& n,int i,int l){
+        if(i==n.size()){
+            return 0;
+        }
+        if(l>=0 and dp[i][l]!=-1){
+            return dp[i][l];
+        }
+        int x=0,y=0;
+        if(i==l+1){
+            x = fun(n,i+1,l);
+        }else{
+            int a = fun(n,i+1,i) + n[i];
+            int b = fun(n,i+1,l);
+            y = max(a,b);
+        }
+        return dp[i][l]=max(x,y);
+    }
+};
