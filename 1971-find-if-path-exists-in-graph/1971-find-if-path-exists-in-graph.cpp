015ef2789db7cf40f671d7c0e9832d5b3d249282
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2022-12-19 21:37:13 +0530

    Time: 1012 ms (66.25%), Space: 126.4 MB (90.02%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& e, int s, int d) {
-        vector<vector<int>> g(n);
+        vector<int> par(n,-1);
         for(auto it:e){
-            int u=it[0],v = it[1];
-            g[u].push_back(v);
-            g[v].push_back(u);
+            int u=it[0],v=it[1];
+            Union(par,u,v);
         }
-        vector<bool> vis(n,0);
-        
-        return fun(g,vis,-1,s,d);
+        int a = find(par,s);
+        int b = find(par,d);
+        return a==b;
     }
-    bool fun(vector<vector<int>>& g,vector<bool>& vis,int par,int i,int d){
-        if(i==d){
-            return 1;
+    void Union(vector<int>& par,int a,int b){
+        int x = find(par,a);
+        int y = find(par,b);
+        
+        if(x==y) return;
+        
+        if(par[x]<par[y]){
+            par[x]+=par[y];
+            par[y] = x;
+        }else{
+            par[y]+=par[x];
+            par[x] = y;
         }
-        vis[i] = 1;
-        for(int x:g[i]){
-            if(x!=par && !vis[x]){
-                bool f= fun(g,vis,i,x,d);
-                if(f) return 1;
-            }
-        }
-        return 0;
+    }
+    int find(vector<int>& par,int a){
+        if(par[a]<0) return a;
+        return par[a] = find(par,par[a]);
     }
 };
