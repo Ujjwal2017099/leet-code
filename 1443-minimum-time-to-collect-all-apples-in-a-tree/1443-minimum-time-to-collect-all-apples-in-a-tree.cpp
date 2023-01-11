642855ac83d7c62d9960d855996824df80a718a8
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2023-01-11 23:28:24 +0530

    Time: 213 ms (70.47%), Space: 65 MB (37.58%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -6,24 +6,8 @@ public:
             g[it[0]].push_back(it[1]);
             g[it[1]].push_back(it[0]);
         }
-        // queue<vector<int>> q;
-        // int ans=0;
-        // q.push({0,0});
-        // map<int,bool> mp;
-        // while(!q.empty()){
-        //     vector<int> x = q.front();q.pop();
-        //     if(ha[x[0]]){
-        //         if(mp[x[1]]) ans++;
-        //         else ans += x[1];
-        //         mp[x[1]] = 1;
-        //     }
-        //     for(int i:g[x[0]]){
-        //         q.push({i,x[1]+1});
-        //     }
-        // }
         vector<bool> vis(n,0);
         int ans = fun(g,vis,0,ha,0);
-        // cout<<ans;
         return ans*2;
     }
     int fun(vector<vector<int>>& g,vector<bool>& vis,int i,vector<bool>& ha,int s){
