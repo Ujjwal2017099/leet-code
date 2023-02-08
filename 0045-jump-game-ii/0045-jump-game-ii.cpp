9d8127f8b93e906846583509f1e6231b42729997
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2023-02-08 11:19:53 +0530

    Time: 16 ms (80.26%), Space: 16.6 MB (84.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& n) {
-        vector<int> dp(n.size(),0);
-        for(int i=n.size()-2;i>=0;i--){
-            for(int j=n[i];j>0;j--){
-                if(j+i==n.size()-1){
-                    dp[i] = 1;
-                }
-                else if(j+i<n.size()){
-                    if(dp[j+i]){
-                        if(dp[i]==0) dp[i] = dp[i+j]+1;
-                        else dp[i] = min(dp[i],dp[i+j]+1);
-                    }
-                }
+        int ans=0;
+        int mx =0,end=0;
+        for(int i=0;i<n.size()-1;i++){
+            mx = max(mx,n[i]+i);
+            
+            if(i==end){
+                end = mx;
+                ans++;
             }
         }
-        // for(int i:dp) cout<<i<<" ";
-        return dp[0];
+        return ans;
     }
 };
