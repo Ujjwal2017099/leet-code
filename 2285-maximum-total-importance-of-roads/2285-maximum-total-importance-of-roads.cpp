29b3887f03b3ab803ac7fddc2ff09d3e1a41865f
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2023-02-24 14:38:30 +0530

    Time: 449 ms (72.07%), Space: 129.2 MB (60.25%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& r) {
+        vector<int> v(n);
+        for(auto i:r){
+            v[i[0]]++;
+            // v[i[0]][1] = i[0];
+            v[i[1]]++;
+            // v[i[1]][1] = i[1];
+        }
+        sort(v.begin(),v.end());
+        long long ans=0;
+        long long cnt=1;
+        for(auto i:v){
+            long long t = i;
+            ans += t*cnt++;
+        }
+        return ans;
+    }
+};
