32762989c2234e8c6760e55436c777f9b68f04a0
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2023-03-01 13:27:17 +0530

    Time: 1034 ms (10.76%), Space: 268.6 MB (5.51%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -3,24 +3,23 @@ public:
     vector<int> sortArray(vector<int>& v) {
         int l=0,r=v.size()-1;
         int md=(l+r)/2;
-        fun(v,l,md);
-        fun(v,md+1,r);
-        int x=md+1;
-        int s=l,e=r;
-        vector<int> t;
-        // for(int i:v) cout<<i<<" ";
-        // cout<<endl;
-        while(l<=md && x<=r){
-            if(v[l]<=v[x]) {t.push_back(v[l]); l++;}
-            else {t.push_back(v[x]);x++;}
-        }
-        while(l<=md){
-            t.push_back(v[l]);l++;
-        }
-        while(x<=r){
-            t.push_back(v[x]);x++;
-        }
-        for(int i=s;i<=e;i++) v[i]=t[i-s];
+        fun(v,l,r);
+        // int x=md+1;
+        // int s=l,e=r;
+        // vector<int> t;
+        // // for(int i:v) cout<<i<<" ";
+        // // cout<<endl;
+        // while(l<=md && x<=r){
+        //     if(v[l]<=v[x]) {t.push_back(v[l]); l++;}
+        //     else {t.push_back(v[x]);x++;}
+        // }
+        // while(l<=md){
+        //     t.push_back(v[l]);l++;
+        // }
+        // while(x<=r){
+        //     t.push_back(v[x]);x++;
+        // }
+        // for(int i=s;i<=e;i++) v[i]=t[i-s];
         
         return v;
     }
