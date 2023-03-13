0c630c88bdda9819cab6e977e820b3fbc14adfb6
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2023-03-13 16:17:35 +0530

    Time: 395 ms (75.00%), Space: 126.7 MB (50.00%) - LeetHub

---
+++ 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long beautifulSubarrays(vector<int>& n) {
+        vector<int> t(n.size());
+        t[0]=n[0];
+        for(int i=1;i<n.size();i++){
+            t[i] = t[i-1] ^ n[i];
+        }
+        map<int,long long> mp;
+        long long ans=0;
+        for(int i=0;i<n.size();i++){
+            ans += mp[t[i]]++;
+            if(t[i]==0) ans++;
+        }
+        
+        return ans;
+    }
+};
