b8742142f22907d370525ed4f5b3beb41c1a5d61
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2023-03-23 15:00:35 +0530

    Time: 16 ms (34.95%), Space: 11.5 MB (44.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -3,11 +3,12 @@ public:
     set<vector<int>> ans;
     vector<vector<int>> combinationSum(vector<int>& c, int t) {
         map<int,int> m;
-        fun(c,t,m);
+        fun(c,t,m,0);
         vector<vector<int>> x(ans.begin(),ans.end());
         return x;
     }
-    void fun(vector<int>& c,int t,map<int,int>& m){
+    void fun(vector<int>& c,int t,map<int,int>& m,int k){
+        // if(k==c.size()) return;
         if(t==0){
             vector<int> x;
             for(auto& it:m){
@@ -18,13 +19,13 @@ public:
             return;
         }
         
-        for(int i=0;i<c.size();i++){
+        for(int i=k;i<c.size();i++){
             if(!m[c[i]]){
                 int x=t/c[i];
                 
                 for(int j=1;j<=x;j++){
                     m[c[i]]=j;
-                    fun(c,t-j*c[i],m);
+                    fun(c,t-j*c[i],m,i+1);
                 }
                 m[c[i]]=0;
             }
