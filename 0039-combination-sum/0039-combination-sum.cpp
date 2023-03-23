e8a766567aaac8fdefdc2d7e4c2031acad489f3a
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2023-03-23 15:13:43 +0530

    Time: 8 ms (58.31%), Space: 10.9 MB (59.99%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -4,31 +4,24 @@ public:
     vector<vector<int>> combinationSum(vector<int>& c, int t) {
         vector<int> m;
         fun(c,t,m,0);
-        // vector<vector<int>> x(ans.begin(),ans.end());
         return ans;
     }
     void fun(vector<int>& c,int t,vector<int>& m,int k){
-        // if(k==c.size()) return;
         if(t==0){
-            // vector<int> x;
-            // for(auto& it:m){
-            //     int y=it.second;
-            //     while(y--) x.push_back(it.first);
-            // }
             ans.push_back(m);
             return;
         }
         
         for(int i=k;i<c.size();i++){
-            // if(!m[c[i]]){
-                int x=t/c[i];
-                
-                for(int j=1;j<=x;j++){
-                    m.push_back(c[i]);
-                    fun(c,t-j*c[i],m,i+1);
-                }
-                while(x--) m.pop_back();
-            // }
+            
+            int x=t/c[i];
+            
+            for(int j=1;j<=x;j++){
+                m.push_back(c[i]);
+                fun(c,t-j*c[i],m,i+1);
+            }
+            while(x--) m.pop_back();
+            
         }
     }
 };
