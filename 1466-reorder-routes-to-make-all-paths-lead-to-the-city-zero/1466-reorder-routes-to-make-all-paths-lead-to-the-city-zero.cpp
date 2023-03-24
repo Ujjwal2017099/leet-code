cbe8c01c9996be5c85651d5ae78d7cadb56be8ef
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2023-03-24 15:18:44 +0530

    Time: 768 ms (15.86%), Space: 149.8 MB (19.55%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& c) {
+        vector<vector<int>> g1(n);
+        vector<vector<int>> g2(n);
+        for(vector<int>& it:c){
+            g1[it[0]].push_back(it[1]);
+            g2[it[1]].push_back(it[0]);
+        }
+        queue<vector<int>> q;q.push({0,0});
+        int ans=0;
+        map<int,bool> mp;
+        while(!q.empty()){
+            vector<int> x=q.front();q.pop();
+            ans+=x[1];
+            mp[x[0]]=1;
+            int j=0;
+            while(j<g1[x[0]].size()){ if(!mp[g1[x[0]][j]]){ q.push({g1[x[0]][j],1});}j++;}
+            j=0;
+            while(j<g2[x[0]].size()){ if(!mp[g2[x[0]][j]]){ q.push({g2[x[0]][j],0});}j++;}
+        }
+        return ans;
+    }
+};
