66cd32704c0d04eb0886565fb8ae08d76bd73110
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2023-04-03 11:15:33 +0530

    Time: 82 ms (83.76%), Space: 42 MB (59.84%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
-    int numRescueBoats(vector<int> people, int limit) {
-        sort(people.begin(),people.end());
-        int count_boat = 0,people_count = 0;
-        int i = 0;
-        int j = people.size() - 1;
+    int numRescueBoats(vector<int>& p, int k) {
+        sort(p.begin(),p.end());
+        int cnt=p.size();
+        int i=0,j=p.size()-1;
         
-        vector<int> remaining;
-        
-        while(people_count < people.size())
-        {
-            if(people[i] + people[j] <= limit)
-            {
-                count_boat++;
-                people_count += 2;
-                i++;
-                j--;
-            }
-            else
-            {
-                remaining.push_back(j);
-                j--;
-            }
-            if(i > j) break;
-            if(i == j){remaining.push_back(i); break;}
+        while(i<j){
+            if(p[i]+p[j]<=k) {cnt--;i++;j--;}
+            else j--;
         }
-        if(remaining.size() == 0)
-        {
-            return count_boat;
-        }
-        else
-        {
-            count_boat += remaining.size();
-            return count_boat;
-        }
+        // if(i<p.size()) cnt++;
+        return cnt;
     }
 };
