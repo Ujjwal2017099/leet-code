4522c5d836c005b8d4d9d1c2c4afeeaaa9053516
Ujjwal Agarwal <100745680+Ujjwal2017099@users.noreply.github.com>
2023-04-11 23:17:53 +0530

    Time: 81 ms (77.67%), Space: 21.9 MB (99.61%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    string removeStars(string s) {
+    string removeStars(string& s) {
         string ans="";
-        for(char c:s){
+        for(char& c:s){
             if(c=='*') ans.pop_back();
             else ans.push_back(c);
         }
