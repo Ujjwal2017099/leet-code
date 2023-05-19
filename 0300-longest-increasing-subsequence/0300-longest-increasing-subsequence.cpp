df9ea4a831575a8f1d97038e2c64d3f52ebba33a
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-05-19 21:04:06 +0530

    Time: 1196 ms (19.96%), Space: 291.2 MB (25.55%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> dp;
+    vector<int> d;
     int lengthOfLIS(vector<int>& n) {
         dp.resize(n.size(),vector<int>(n.size(),-1));
+        d.resize(n.size(),0);
         int ans=0;
         for(int i=0;i<n.size();i++){
+            if(d[i]) continue;
             ans=max(ans,fun(n,i+1,i)+1);
         }
         return ans;
@@ -20,6 +23,8 @@ public:
             return dp[l][i]=fun(n,i+1,l);
         }
         
-        return dp[l][i] = max(fun(n,i+1,i)+1,fun(n,i+1,l));
+        dp[l][i] = max(fun(n,i+1,i)+1,fun(n,i+1,l));
+        d[i]=1;
+        return dp[l][i];
     }
 };
