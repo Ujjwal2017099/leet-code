c3cb424977bf05b84e4b4652b3af8e234c2cd997
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-05-25 00:18:28 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (47.69%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -3,21 +3,21 @@ public:
     vector<int> getRow(int r) {
         if(r==0) {return {1};}
         
-        vector<int> t(2,1);
+        vector<int> t(r+1,1);
         for(int j=2;j<=r;j++){
-            vector<int> x;
-            x.push_back(1);
+            int prev=1;
             for(int i=1;i<j;i++){
-                x.push_back(t[i-1]+t[i]);
+                int x=t[i];
+                t[i]+=(prev);
+                prev=x;
             }
-            x.push_back(1);
-            for(int i=0;i<x.size();i++){
-                if(i>=t.size()){
-                    t.push_back(x[i]);
-                }else{
-                    t[i]=x[i];
-                }
-            }
+            // for(int i=0;i<x.size();i++){
+            //     if(i>=t.size()){
+            //         t.push_back(x[i]);
+            //     }else{
+            //         t[i]=x[i];
+            //     }
+            // }
         }
         return t;
     }
