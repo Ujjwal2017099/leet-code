1caa082620c6e830323441bc040c6b6cd7597930
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-05-24 23:42:23 +0530

    Time: 3 ms (62.23%), Space: 8.9 MB (99.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
+    void merge(vector<int>& n1, int& m, vector<int>& n2, int& n) {
         int i=m-1,j=n-1;
         while(i>=0 && j>=0){
             if(n1[i]>=n2[j]){
