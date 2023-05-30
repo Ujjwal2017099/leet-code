fc9ff62d28e372dbdb714a40fc63acb13f6b5288
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-05-30 14:28:54 +0530

    Time: 91 ms (5.21%), Space: 29 MB (6.83%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& n) {
+        int ans=INT_MAX;
+        int l=0;
+        vector<int> t(n.size()+1,0);
+        partial_sum(n.begin(),n.end(),n.begin());
+        if(n.back()>=target) ans=n.size();
+        for(int i=0;i<n.size();i++){
+            while(l<i && n[i]-n[l]>=target){
+                ans=min(ans,i-l);
+                cout<<l<<" "<<i<<endl;
+                l++;
+                // cout<<i-l+1<<" ";
+            }
+            if(n[i]>=target){
+                ans=min(ans,i+1);
+            }
+        }
+        while(l<n.size() && n.back()-n[l]>=target){
+            int i=n.size();
+            ans=min(ans,i-l);l++;
+            cout<<l<<" "<<i<<endl;
+        }
+        return ans==INT_MAX ? 0 : ans;
+    }
+};
