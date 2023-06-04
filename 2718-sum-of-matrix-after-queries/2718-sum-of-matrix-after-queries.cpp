d1ebfe50331b7cf606ecd3d8d0db53c4dc0e218c
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-06-04 17:29:05 +0530

    Time: 603 ms (9.09%), Space: 191.1 MB (9.09%) - LeetHub

--- 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.cpp
+++ 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.cpp
@@ -3,27 +3,24 @@ public:
     long long matrixSumQueries(int n, vector<vector<int>>& q) {
         long long ans=0;
         long long r=n,c=n;
-        unordered_map<int,bool> mr;
-        unordered_map<int,bool> mc;
+        
+        vector<vector<bool>> mp(2,vector<bool>(n,0));
         
         for(int i=q.size()-1;i>=0;i--){
             if(!r && !c) break;
             int x=q[i][0];
             int y=q[i][1];
             long long val=q[i][2];
-            if(x && mc[y]) continue;
-            if(!x && mr[y]) continue;
+            if(mp[x][y]) continue;
             
             if(x && r){
                 ans += r*val;c--;
-                mc[y]=1;
             }
             if(!x && c){
                 ans += c*val;r--;
-                mr[y]=1;
             }
             
-            
+            mp[x][y]=1;
         }
         return ans;
     }
