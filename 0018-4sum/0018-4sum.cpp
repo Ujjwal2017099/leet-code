4fc39e4cf0c4b25701ef8824c8d16525793ff081
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-07-23 00:29:23 +0530

    Time: 1167 ms (9.80%), Space: 136.4 MB (9.41%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& n, int t) {
+        sort(n.begin(),n.end());
+        unordered_map<long long,int> m;
+        for(int i=0;i<n.size();i++) m[n[i]]=i+1;
+        vector<vector<int>> x;
+        if(n.size()<4) return x;
+        for(int i=0;i<n.size()-2;i++){
+            for(int j=i+1;j<n.size()-1;j++){
+                for(int k=j+1;k<n.size();k++){
+                    long long y = t*1LL - (n[i]*1LL+n[j]*1LL+n[k]*1LL);
+                    int a = m[y];
+                    // cout<<a<<" ";
+                    if(a>k+1){
+                        x.push_back({n[i],n[j],n[k],n[a-1]});
+                    }
+                    while(k<n.size()-1 && n[k+1]==n[k]) k++;
+                }
+                while(j<n.size()-1 && n[j+1]==n[j]) j++;
+            }
+            while(i<n.size()-1 && n[i+1]==n[i]) i++;
+        }
+        return x;
+    }
+};
