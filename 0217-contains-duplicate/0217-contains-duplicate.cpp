7dd09ad4687bec1d6baf4cf719dfe8582af05c29
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-07-30 23:10:04 +0530

    Time: 91 ms (99.50%), Space: 57.3 MB (72.78%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& n) {
+        sort(n.begin(),n.end());
+        for(int i=1;i<n.size();i++) if(n[i]==n[i-1]) return 1;
+        return 0;
+    }
+};
