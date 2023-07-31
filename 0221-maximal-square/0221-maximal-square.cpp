5f19c8dfaa81438b40a83c09cd3f918ef0ec459b
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-08-01 00:53:12 +0530

    Time: 65 ms (99.36%), Space: 21 MB (35.66%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& m) {
-        vector<vector<int>> dp(m.size(),vector<int>(m[0].size()+1,0));
-        for(int i=0;i<m.size();i++){
-            for(int j=0;j<m[0].size();j++){
-                if(m[i][j]=='1') dp[i][j+1]=1;
-            }
-        }
-        for(int i=0;i<dp.size();i++){
-            for(int j=1;j<dp[0].size();j++){
-                dp[i][j] += dp[i][j-1];
-            }
-        }
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
         int ans=0;
-        for(int i=0;i<dp.size();i++){
-            for(int j=0;j<dp[0].size();j++){
-                int a=j;
-                for(int k=j+1;k<dp[0].size();k++){
-                    j=k-1;
-                    if(dp[i][k]-dp[i][a]==k-a){
-                        int x=k-a;
-                        for(int y=i;y<dp.size() && x;y++){
-                            if(dp[y][k]-dp[y][a]==k-a) x--;
-                            else break;
-                        }
-                        for(int y=i-1;y>=0 && x;y--){
-                            if(dp[y][k]-dp[y][a]==k-a) x--;
-                            else break;
-                        }
-                        if(!x) ans=max(ans,(k-a)*(k-a));
-                        else {break;}
-                    }else{
-                        break;
-                    }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(!j or !i or matrix[i][j]=='0')
+                {
+                    dp[i][j]=matrix[i][j]-'0';
                 }
+                else
+                    dp[i][j]=min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
+                ans=max(ans,dp[i][j]);
             }
-        }
-        return ans;
+        }return ans*ans;
     }
 };
