218dd240066d98c08d6492f878b59725f3a5e469
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-08-01 16:59:57 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (53.64%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        sort(c.begin(),c.end());
+        int ans=0;
+        for(int i=1;i<=c.size();i++){
+            auto it=lower_bound(c.begin(),c.end(),i);
+            if(it==c.end()) break;
+            int x=it-c.begin();
+            if(c.size()-x>=i) ans=i;
+            else break;
+        }
+        return ans;
+    }
+};
