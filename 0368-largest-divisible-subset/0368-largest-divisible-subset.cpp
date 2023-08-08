db5c4b3aa4d5fd4b44bb4a5c720795deb467afa7
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-08-08 20:53:51 +0530

    Time: 204 ms (5.13%), Space: 63.1 MB (5.05%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& n) {
+        sort(n.begin(),n.end());
+        
+        vector<vector<int>> dp(n.size()+1,vector<int>(n.size(),0));
+        for(int i=0;i<n.size();i++){
+            for(int j=i+1;j<n.size();j++){
+                if(n[j]%n[i]==0) dp[i+1][j]=max(dp[i][i]+1,dp[i][j]);
+                else dp[i+1][j]=dp[i][j];
+            }
+        }
+        int mx=0;
+        for(int i=0;i<n.size();i++){
+            for(int j=0;j<n.size();j++){
+                if(dp[i+1][j]>=dp[mx][mx]){
+                    mx=j;       
+                }
+                // cout<<dp[i+1][j]<<" ";
+            }
+            // cout<<endl;
+        }
+        vector<int> ans;
+        ans.push_back(n[mx]);
+        int prev=mx;
+        while(mx>0){
+            if(ans.back()%n[mx-1]==0 && dp[mx][prev]>dp[mx-1][prev]){
+                prev=mx-1;
+                ans.push_back(n[prev]);
+            }
+            mx--;
+        }
+        // if(n[0])
+        return ans;;
+    }
+    
+};
