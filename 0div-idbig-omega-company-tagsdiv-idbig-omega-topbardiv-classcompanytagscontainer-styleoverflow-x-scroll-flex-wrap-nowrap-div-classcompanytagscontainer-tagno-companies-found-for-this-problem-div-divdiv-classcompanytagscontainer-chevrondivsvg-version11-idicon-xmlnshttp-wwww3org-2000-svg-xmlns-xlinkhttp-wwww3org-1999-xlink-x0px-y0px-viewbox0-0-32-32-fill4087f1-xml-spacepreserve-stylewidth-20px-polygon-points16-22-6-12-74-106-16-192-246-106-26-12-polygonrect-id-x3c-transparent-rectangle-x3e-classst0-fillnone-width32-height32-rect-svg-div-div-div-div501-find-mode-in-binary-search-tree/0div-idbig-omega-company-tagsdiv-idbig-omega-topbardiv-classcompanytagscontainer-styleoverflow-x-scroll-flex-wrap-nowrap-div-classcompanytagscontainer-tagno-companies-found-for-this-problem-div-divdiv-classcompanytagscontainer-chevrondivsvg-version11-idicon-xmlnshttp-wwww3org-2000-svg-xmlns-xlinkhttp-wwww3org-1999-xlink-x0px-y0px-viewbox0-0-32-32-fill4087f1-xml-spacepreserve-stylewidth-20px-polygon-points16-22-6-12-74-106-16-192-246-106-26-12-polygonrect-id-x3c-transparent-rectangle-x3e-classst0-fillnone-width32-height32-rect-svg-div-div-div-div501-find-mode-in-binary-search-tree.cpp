af5cb4361a27ab1955ea92098b00a25ce94fb2a5
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2023-11-01 17:06:47 +0530

    Time: 23 ms (18.88%), Space: 25.9 MB (27.04%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div501-find-mode-in-binary-search-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mx=0;
+    map<int,int> cnt;
+    vector<int> findMode(TreeNode* root) {
+        vector<int> ans;
+        fun(root);
+        for(auto it:cnt){
+            if(it.second==mx)ans.push_back(it.first);
+        }
+        return ans;
+    }
+    void fun(TreeNode* node){
+        if(!node) return;
+        
+        mx=max(mx,++cnt[node->val]);
+        
+        fun(node->left);
+        fun(node->right);
+    }
+};
