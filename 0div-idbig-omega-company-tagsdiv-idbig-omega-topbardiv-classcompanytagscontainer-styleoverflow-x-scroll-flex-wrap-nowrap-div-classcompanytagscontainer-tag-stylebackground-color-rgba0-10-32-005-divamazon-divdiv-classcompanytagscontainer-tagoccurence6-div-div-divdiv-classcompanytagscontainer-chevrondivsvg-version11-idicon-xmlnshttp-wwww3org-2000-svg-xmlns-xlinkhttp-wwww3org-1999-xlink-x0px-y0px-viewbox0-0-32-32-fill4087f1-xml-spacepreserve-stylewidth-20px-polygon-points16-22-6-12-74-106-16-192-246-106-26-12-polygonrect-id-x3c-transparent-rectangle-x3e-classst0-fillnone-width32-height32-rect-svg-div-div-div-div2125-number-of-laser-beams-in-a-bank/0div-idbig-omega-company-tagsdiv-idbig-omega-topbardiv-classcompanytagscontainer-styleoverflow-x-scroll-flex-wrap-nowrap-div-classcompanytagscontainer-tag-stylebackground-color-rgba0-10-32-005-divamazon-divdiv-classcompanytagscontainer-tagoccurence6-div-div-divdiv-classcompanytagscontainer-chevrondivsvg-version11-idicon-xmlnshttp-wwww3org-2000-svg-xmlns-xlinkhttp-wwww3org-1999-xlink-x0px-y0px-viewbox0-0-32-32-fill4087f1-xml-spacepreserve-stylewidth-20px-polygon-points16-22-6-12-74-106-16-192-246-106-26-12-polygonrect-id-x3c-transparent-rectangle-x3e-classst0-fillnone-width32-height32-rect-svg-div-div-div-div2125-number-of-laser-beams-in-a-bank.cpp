2e78529da0805cb73e63a9b18d28a3c1cd46b0a9
Ujjwal Agarwal <agarwal.ujjwal.2017099@gmail.com>
2024-01-03 19:56:15 +0530

    Time: 84 ms (79.88%), Space: 29.6 MB (5.06%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2125-number-of-laser-beams-in-a-bank/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& b) {
+        int ans=0;
+        vector<int> v;
+        for(string s:b){
+            int cnt=0;
+            for(int i=0;i<s.size();i++){
+                if(s[i]=='1') cnt++;
+            }
+            v.push_back(cnt);
+            // cout<<cnt;
+        }
+        int prev=0;
+        if(v.size()){
+            prev=v[0];
+        }
+        bool f=0;
+        for(int i=1;i<v.size();i++){
+            if(v[i] ){
+                ans += prev*v[i];
+                prev=v[i];
+            }
+            // if(v[i]) prev=v[i];
+            
+        }
+        return ans;
+    }
+};
