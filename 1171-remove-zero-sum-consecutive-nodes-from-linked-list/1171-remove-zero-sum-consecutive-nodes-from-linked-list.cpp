8164989523eaa6ead903e3a829c6b18f37e9ff64
Ujjwal Agarwal <ujjwalagarwal863@gmail.com>
2024-03-12 03:01:36 -0700

    Time: 8 ms (58.31%), Space: 13.2 MB (76.62%) - LeetHub

---
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -0,0 +1,37 @@
+
+class Solution {
+public:
+    ListNode* t = new ListNode(1e7);
+    ListNode* removeZeroSumSublists(ListNode* h) {
+        t->next = h;
+        bool f=1;
+        
+        fun( h,1e7,t);
+        
+        return t->next;
+    }
+    void fun(ListNode* ptr,int sm,ListNode* prev){
+        if(!ptr) return;
+        
+        int x = ptr->val;
+        int nxt_sm=sm;
+        // if(x<=0 && sm+x>=0){
+            ListNode* temp = prev;
+            bool f=0;
+            while(sm+x!=0 && temp){
+                sm-=temp->val;
+                if(f) prev=prev->next;
+                temp=temp->next;
+                f=1;
+            }
+            if(sm+x==0){
+                prev->next=ptr->next;
+                nxt_sm -= sm; 
+            }else{
+                nxt_sm+=x;
+            }
+        // }
+        // else{nxt_sm+=x;}
+        fun(ptr->next,nxt_sm,t);
+    }
+};
