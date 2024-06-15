8c2ca195aa521401f955f9e59c493891215d8555
Ujjwal Agarwal <ujjwalagarwal863@gmail.com>
2024-06-15 18:36:01 +0530

    Time: 377 ms (92.80%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
 
-select max(cnt) as num from (select if(count(num)=1 , num,0.5) as cnt from MyNumbers group by num) as T
-where cnt != 0.5
+select max(num) as num from (select num from MyNumbers group by num having count(num)=1) as T
